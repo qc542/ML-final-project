cbd654087b79285bd50b4b1f1541be4823559c24
qc542 <36024574+qc542@users.noreply.github.com>
2020-11-25 22:59:03 -0500

    More work on unsupervised analysis Added code that converts features written as strings to numbers. Added code that creates more graphs of various relations present in the dataset.

--- unsupervised_analysis.py
+++ unsupervised_analysis.py
@@ -4,18 +4,30 @@ import pandas as pd
 
 dataset = pd.read_csv("AB_NYC_2019.csv")
 dataset = dataset.dropna()
+
+# Convert features written as strings to numbers
 for i in dataset.index:
     if dataset['room_type'][i] == "Entire home/apt":
         dataset.at[i, 'room_type'] = 1
-    elif dataset['room_type'][i] == "Private room":
+    if dataset['room_type'][i] == "Private room":
         dataset.at[i, 'room_type'] = 2
-    elif dataset['room_type'][i] == "Shared room":
+    if dataset['room_type'][i] == "Shared room":
         dataset.at[i, 'room_type'] = 3
+    if dataset['neighbourhood_group'][i] == "Manhattan":
+        dataset.at[i, 'neighbourhood_group'] = 1
+    if dataset['neighbourhood_group'][i] == "Brooklyn":
+        dataset.at[i, 'neighbourhood_group'] = 2
+    if dataset['neighbourhood_group'][i] == "Bronx":
+        dataset.at[i, 'neighbourhood_group'] = 3
+    if dataset['neighbourhood_group'][i] == "Staten Island":
+        dataset.at[i, 'neighbourhood_group'] = 4
+    if dataset['neighbourhood_group'][i] == "Queens":
+        dataset.at[i, 'neighbourhood_group'] = 5
 
 dataset = dataset[['latitude', 'longitude', 'price', 'minimum_nights',\
         'number_of_reviews', 'reviews_per_month', \
         'calculated_host_listings_count', 'availability_365', \
-        'room_type']]
+        'room_type', 'neighbourhood_group']]
 model = KMeans(n_clusters=5)
 model.fit(dataset)
 
@@ -42,4 +54,59 @@ legend = plt.legend(*scatter.legend_elements(), loc="lower left", \
 ax.add_artist(legend)
 plt.xlabel('Reviews per Month')
 plt.ylabel('Price ($)')
+plt.show()
+
+y_axis = dataset['price']
+x_axis = dataset['minimum_nights']
+fig, ax = plt.subplots()
+scatter = ax.scatter(x_axis, y_axis, c=all_predictions)
+legend = plt.legend(*scatter.legend_elements(), loc="lower left", \
+        title="Clusters")
+ax.add_artist(legend)
+plt.xlabel('Minimum # of Nights')
+plt.ylabel('Price ($)')
+plt.show()
+
+y_axis = dataset['price']
+x_axis = dataset['neighbourhood_group']
+fig, ax = plt.subplots()
+scatter = ax.scatter(x_axis, y_axis, c=all_predictions)
+legend = plt.legend(*scatter.legend_elements(), loc="lower left", \
+        title="Clusters")
+ax.add_artist(legend)
+plt.xlabel('Neighbourhood Group')
+plt.ylabel('Price ($)')
+plt.show()
+
+y_axis = dataset['price']
+x_axis = dataset['room_type']
+fig, ax = plt.subplots()
+scatter = ax.scatter(x_axis, y_axis, c=all_predictions)
+legend = plt.legend(*scatter.legend_elements(), loc="lower left", \
+        title="Clusters")
+ax.add_artist(legend)
+plt.xlabel('Room Type')
+plt.ylabel('Price ($)')
+plt.show()
+
+y_axis = dataset['room_type']
+x_axis = dataset['minimum_nights']
+fig, ax = plt.subplots()
+scatter = ax.scatter(x_axis, y_axis, c=all_predictions)
+legend = plt.legend(*scatter.legend_elements(), loc="lower left", \
+        title="Clusters")
+ax.add_artist(legend)
+plt.xlabel('Minimum # of Nights')
+plt.ylabel('Room Type')
+plt.show()
+
+y_axis = dataset['number_of_reviews']
+x_axis = dataset['minimum_nights']
+fig, ax = plt.subplots()
+scatter = ax.scatter(x_axis, y_axis, c=all_predictions)
+legend = plt.legend(*scatter.legend_elements(), loc="lower left", \
+        title="Clusters")
+ax.add_artist(legend)
+plt.xlabel('Minimum # of Nights')
+plt.ylabel('Number of Reviews')
 plt.show()
